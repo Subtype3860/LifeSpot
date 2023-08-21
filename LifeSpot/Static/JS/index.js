e1b23073152129ca185cad2244f9d576be4f7c39
Subtype3860 <booravoi@gmail.com>
2023-08-21 22:05:42 +0300

    update project

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -48,10 +48,26 @@ function filterContent() {
     }
 }
 
-/*
-* Всплывающее окно будет показано по таймауту
-* 
-* */
-setTimeout(() =>
-    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
-    30000);
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+
